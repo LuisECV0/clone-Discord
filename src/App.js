ea8f91cc37f216a0fa99b881dd9802fdbcdb80f8
Luiscv1719 <luiscv1719@gmail.com>
2024-06-17 23:32:48 -0500

    cambios en el fondo y las imagenes flotantes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@
  import Header from './components/header.js';
  import Body from './components/body.js';
 
-
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <Header />
+      <div className="background-icons">
+          <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664b1d55ec9b7c26d37eceed_dis_Icon_Crown%20(1).webp" alt="" className="icon crown-icon" />
+          <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/662632b7f75202aa67c25126_turnip.webp" alt="" className="icon turnip-icon" />
+          <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664b1d8fe64b8c656d8da82f_potion.webp" alt="" className="icon potion-icon" />
+          <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/662632b7f75202aa67c25126_turnip.webp" alt="" className="icon turnip-icon" />
+          <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664b1d8fe64b8c656d8da82f_potion.webp" alt="" className="icon potion-icon" />
+      </div>
       <Body />
     </div>
   );
 }
-
 export default App;
--- src/components/body.js
+++ src/components/body.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Styles/Body.css';
 
-function body() {
+function Body() {
   return (
     <div className="body">
-      <div className="background-icons">
-        <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664b1d55ec9b7c26d37eceed_dis_Icon_Crown%20(1).webp" alt="" className="icon crown-icon" />
-        <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/662632b7f75202aa67c25126_turnip.webp" alt="" className="icon turnip-icon" />
-        <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664b1d8fe64b8c656d8da82f_potion.webp" alt="" className="icon potion-icon" />
-        <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/662632b7f75202aa67c25126_turnip.webp" alt="" className="icon turnip-icon" />
-        <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664b1d8fe64b8c656d8da82f_potion.webp" alt="" className="icon potion-icon" />
-      </div>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>UN CHAT DE</h1>
+          <h2>GRUPO LLENO DE JUEGOS Y DIVERSIÓN</h2>
+          <p>Discord es la plataforma perfecta para jugar y pasarlo bien con amigos, o incluso para crear una comunidad con miembros en todo el mundo.</p>
+        </div>
+      </section>
+      <img href='./Resources/discord.webp'></img>
     </div>
   );
 }
 
-export default body;
+export default Body;
